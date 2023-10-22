a4a33671d9a30c3e0f1cf46fd7f9b77d58ff9bd6
ctag07 <gamingman313@gmail.com>
2023-10-22 12:43:04 -0400

    Make start button trigger properly?

--- MultiHustle/ui/SteamLobby/LobbyUser.gd
+++ MultiHustle/ui/SteamLobby/LobbyUser.gd
@@ -10,11 +10,7 @@ func init(member):
 	elif Steam.getLobbyOwner(SteamLobby.LOBBY_ID) == SteamHustle.STEAM_ID:
 		button.show()
 		button.text = "Start Game"
-		if button.is_connected("pressed", self, "on_challenge_pressed"):
-			button.disconnect("pressed", self, "on_challenge_pressed")
-			button.connect("pressed", self, "on_start_game_pressed")
-			print("[MultiHustle] Start game button signal connected")
 
-func on_start_game_pressed():
+func on_challenge_pressed():
 	emit_signal("start_game_pressed")
 	SteamLobby.host_game_vs_all()
